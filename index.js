083a0a28f0389aa21f4f7c0535cba0d319c79f8c
yml <yuemenglong@126.com>
2016-01-19 15:57:10 +0800

    add all module

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
 var Exception = require("./common/exception");
 var HttpClient = require("./common/http_client");
 var kit = require("./common/kit");
+var logger = require("./common/logger");
 var loop = require("./common/loop");
 var promise = require("./common/promise");
+var Reader = require("./common/reader");
 var rpc = require("./common/rpc");
+var Scheduler = require("./common/scheduler");
+var Task = require("../common/task");
 var time = require("./common/time");
 
 module.exports = {
     Exception: Exception,
     HttpClient: HttpClient,
     kit: kit,
+    logger: logger,
     loop: loop,
     promise: promise,
+    Reader: reader,
     rpc: rpc,
+    Scheduler: Scheduler,
+    Task: Task,
     time: time,
 }
