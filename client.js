7f8ba911fd6f03567786f899664fe91b09de5f60
menglong yue <menglong yue>
2016-02-10 21:07:47 +0800

    use event to resolve and reject

--- client.js
+++ client.js
@@ -4,21 +4,6 @@ var logger = common.logger;
 var Promise = require("bluebird");
 
 var socket = new Socket();
-socket.connect("localhost", 80).then(function() {
+socket.connect("61.135.169.121", 80).then(function() {
     socket.close();
-}).then(function() {
-	logger.log(1);
-    return Promise.delay(3000);
-}).then(function() {
-	logger.log(2);
-    return socket.read(1);
-}).then(function() {
-	logger.log(3);
-    return Promise.delay(1000000);
-}).catch(function(err) {
-	logger.log(4);
-    logger.log(err);
-}).then(function() {
-	logger.log(5);
-	
 }).done();
