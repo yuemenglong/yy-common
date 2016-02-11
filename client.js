8acd72427635ba1bd358544e96432b39d9a89444
menglong yue <menglong yue>
2016-02-11 09:20:58 +0800

    unused

--- client.js
+++
@@ -1,9 +0,0 @@
-var common = require(".");
-var Socket = common.Socket;
-var logger = common.logger;
-var Promise = require("bluebird");
-
-var socket = new Socket();
-socket.connect("61.135.169.121", 80).then(function() {
-    socket.close();
-}).done();
